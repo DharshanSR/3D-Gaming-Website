05fbad2c98117cbfa3f2a26c334a936664392c0d
DharshanSR <dharshanravindran8@gmail.com>
2024-12-18 21:57:20 +0530

    change some error fixing abd bug fixing

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import About from './components/About';
-import Features from './components/Features';
 import Hero from './components/Hero';
 import NavBar from './components/Navbar';
 import Contact from "./components/Contact.jsx";
 import Footer from "./components/Footer.jsx";
 import Story from "./components/Story.jsx";
+import Features from "./components/Features.jsx";
 
 function App() {
 
     return (
         <main className='relative min-h-screen w-screen overflow-x-hidden'>
-            <NavBar/>
-            <Hero/>
-            <About/>
-      <Feature />
-      <Story />
-      <Contact />
-        <Footer />
+            <NavBar />
+            <Hero />
+            <About />
+            <Features />
+            <Story />
+            <Contact />
+            <Footer />
         </main>
     )
 }
