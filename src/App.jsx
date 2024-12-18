182ad398dbc35b71efce448175f5189c50c8344b
DharshanSR <dharshanravindran8@gmail.com>
2024-12-18 20:55:55 +0530

    feat: Add interactive 'Story' section with animated elements and immersive visuals

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import About from './components/About';
 import Hero from './components/Hero';
 import NavBar from './components/Navbar';
+import Story from "./components/Story.jsx";
 
 function App() {
 
-  return (
-    <main className='relative min-h-screen w-screen overflow-x-hidden'>
-      <NavBar />
-      <Hero />
-      <About />
-    </main>
-  )
+    return (
+        <main className='relative min-h-screen w-screen overflow-x-hidden'>
+            <NavBar/>
+            <Hero/>
+            <About/>
+            <Story/>
+        </main>
+    )
 }
 
 export default App
